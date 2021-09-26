0c584639aff03bace2dd12e3249a82c50363c77a
Wesley Snijdelaar <wesleysnijdelaar@outlook.com>
2021-09-26 12:14:26 +0200

    Faster blockstate lookup for mobspawning

--- src/main/java/org/provim/servercore/mixin/performance/mob_spawning/SpawnHelperMixin.java
+++ src/main/java/org/provim/servercore/mixin/performance/mob_spawning/SpawnHelperMixin.java
@@ -1,10 +1,12 @@
 package org.provim.servercore.mixin.performance.mob_spawning;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.WorldChunk;
 import org.provim.servercore.utils.ChunkManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(SpawnHelper.class)
 public abstract class SpawnHelperMixin {
     @Unique
-    private static Chunk cachedChunk;
+    private static WorldChunk cachedChunk;
 
     private SpawnHelperMixin() {
     }
@@ -34,6 +36,12 @@ public abstract class SpawnHelperMixin {
     @Redirect(method = "spawnEntitiesInChunk(Lnet/minecraft/entity/SpawnGroup;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/world/chunk/Chunk;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/SpawnHelper$Checker;Lnet/minecraft/world/SpawnHelper$Runner;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/SpawnHelper;isAcceptableSpawnPosition(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/world/chunk/Chunk;Lnet/minecraft/util/math/BlockPos$Mutable;D)Z"))
     private static boolean onlySpawnIfLoaded(ServerWorld world, Chunk chunk, BlockPos.Mutable pos, double squaredDistance) {
         return isAcceptableSpawnPosition(world, chunk, pos, squaredDistance) && (cachedChunk = ChunkManager.getChunkIfLoaded(world, pos)) != null;
+    }
+
+    // Fast blockstate lookup.
+    @Redirect(method = "getSpawnEntries", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;"))
+    private static BlockState fastBlockStateLookup(ServerWorld serverWorld, BlockPos pos) {
+        return cachedChunk.getBlockState(pos);
     }
 
     // Fast biome lookups.
