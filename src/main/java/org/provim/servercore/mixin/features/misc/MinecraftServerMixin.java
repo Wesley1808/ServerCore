6bd54f4e876d25546243d0496c46f892821ff4b9
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2022-01-06 14:58:02 +0100

    Fix #15 - Datapacks (and more) breaking when spawn chunks are disabled.

--- src/main/java/org/provim/servercore/mixin/features/misc/MinecraftServerMixin.java
+++ src/main/java/org/provim/servercore/mixin/features/misc/MinecraftServerMixin.java
@@ -1,23 +1,29 @@
 package org.provim.servercore.mixin.features.misc;
 
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.server.level.ServerChunkCache;
+import net.minecraft.server.level.TicketType;
+import net.minecraft.world.level.ChunkPos;
 import org.provim.servercore.config.tables.FeatureConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = MinecraftServer.class, priority = 900)
 public abstract class MinecraftServerMixin {
 
-    @Inject(method = "prepareLevels", at = @At("HEAD"), cancellable = true)
-    private void disableSpawnChunks(ChunkProgressListener chunkProgressListener, CallbackInfo ci) {
-        if (FeatureConfig.DISABLE_SPAWN_CHUNKS.get()) {
-            ci.cancel();
+    @Redirect(method = "prepareLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerChunkCache;addRegionTicket(Lnet/minecraft/server/level/TicketType;Lnet/minecraft/world/level/ChunkPos;ILjava/lang/Object;)V"))
+    private <T> void disableSpawnChunks(ServerChunkCache chunkCache, TicketType<T> ticketType, ChunkPos chunkPos, int i, T object) {
+        if (!FeatureConfig.DISABLE_SPAWN_CHUNKS.get()) {
+            chunkCache.addRegionTicket(ticketType, chunkPos, i, object);
         }
+    }
+
+    @Redirect(method = "prepareLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerChunkCache;getTickingGenerated()I"))
+    private int disableSpawnChunks(ServerChunkCache chunkCache) {
+        return FeatureConfig.DISABLE_SPAWN_CHUNKS.get() ? 441 : chunkCache.getTickingGenerated();
     }
 
     @ModifyConstant(method = "tickServer", constant = @Constant(intValue = 6000), require = 0)
