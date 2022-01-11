d3ecbf30463ea8eab7d4f138cd47bd558825876e
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2022-01-11 15:36:02 +0100

    Update dependencies & support for modmenu

--- src/main/java/me/wesley1808/servercore/mixin/events/PlayerListMixin.java
+++ src/main/java/me/wesley1808/servercore/mixin/events/PlayerListMixin.java
@@ -10,18 +10,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerList.class)
 public abstract class PlayerListMixin {
 
-    @Inject(
-            method = "setViewDistance",
-            at = @At("HEAD")
-    )
+    @Inject(method = "setViewDistance", at = @At("HEAD"))
     private void updateViewDistance(int i, CallbackInfo ci) {
         TickManager.setViewDistance(i);
     }
 
-    @Inject(
-            method = "setSimulationDistance",
-            at = @At("HEAD")
-    )
+    @Inject(method = "setSimulationDistance", at = @At("HEAD"))
     private void updateSimulationDistance(int i, CallbackInfo ci) {
         TickManager.setSimulationDistance(i);
     }
