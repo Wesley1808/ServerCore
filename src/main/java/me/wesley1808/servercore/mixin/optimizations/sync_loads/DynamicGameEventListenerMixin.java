86ca4eb89ef4af97281b66208a7eedc6ac6ec9ef
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2022-03-31 21:50:34 +0200

    Fix: mojank - MC-249712

---
+++ src/main/java/me/wesley1808/servercore/mixin/optimizations/sync_loads/DynamicGameEventListenerMixin.java
@@ -0,0 +1,26 @@
+package me.wesley1808.servercore.mixin.optimizations.sync_loads;
+
+import me.wesley1808.servercore.utils.ChunkManager;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ChunkStatus;
+import net.minecraft.world.level.gameevent.DynamicGameEventListener;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(DynamicGameEventListener.class)
+public abstract class DynamicGameEventListenerMixin {
+
+    @Redirect(method = "ifChunkExists",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/LevelReader;getChunk(IILnet/minecraft/world/level/chunk/ChunkStatus;Z)Lnet/minecraft/world/level/chunk/ChunkAccess;"
+            )
+    )
+    private static ChunkAccess getChunkIfLoaded(LevelReader levelReader, int x, int z, ChunkStatus status, boolean bl) {
+        // levelReader is always an instance of ServerLevel here.
+        return ChunkManager.getChunkIfLoaded((Level) levelReader, x, z);
+    }
+}
