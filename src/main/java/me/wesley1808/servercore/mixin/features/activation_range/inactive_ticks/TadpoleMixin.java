dd3b20e3785587889d2563f6e3627d158953d561
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2022-05-03 20:22:31 +0200

    Fixed: tadpoles do not age when inactive.

---
+++ src/main/java/me/wesley1808/servercore/mixin/features/activation_range/inactive_ticks/TadpoleMixin.java
@@ -0,0 +1,30 @@
+package me.wesley1808.servercore.mixin.features.activation_range.inactive_ticks;
+
+import me.wesley1808.servercore.interfaces.activation_range.InactiveEntity;
+import me.wesley1808.servercore.utils.ActivationRange;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.AbstractFish;
+import net.minecraft.world.entity.animal.frog.Tadpole;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Tadpole.class)
+public abstract class TadpoleMixin extends AbstractFish implements InactiveEntity {
+    @Shadow
+    private int age;
+
+    private TadpoleMixin(EntityType<? extends AbstractFish> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Shadow
+    protected abstract void setAge(int i);
+
+    @Override
+    public void inactiveTick() {
+        this.noActionTime++;
+        this.setAge(this.age + 1);
+        ActivationRange.updateGoalSelectors(this.goalSelector, this.targetSelector);
+    }
+}
