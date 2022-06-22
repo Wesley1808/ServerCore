09ffb1b7d1c958b4bb2723d4ef1f3ce09af59a4b
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2022-06-22 11:27:28 +0200

    Allow chickens to lay eggs during inactive ticks.

--- src/main/java/me/wesley1808/servercore/mixin/features/activation_range/inactive_ticks/ChickenMixin.java
+++ src/main/java/me/wesley1808/servercore/mixin/features/activation_range/inactive_ticks/ChickenMixin.java
@@ -2,10 +2,13 @@ package me.wesley1808.servercore.mixin.features.activation_range.inactive_ticks;
 
 import me.wesley1808.servercore.common.interfaces.activation_range.InactiveEntity;
 import me.wesley1808.servercore.common.utils.ActivationRange;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -23,8 +26,12 @@ public abstract class ChickenMixin extends Animal implements InactiveEntity {
 
     @Override
     public void inactiveTick() {
-        if (!this.isChickenJockey && this.age >= 0 && this.isAlive()) {
-            --this.eggTime;
+        if (!this.isChickenJockey && this.age >= 0 && this.isAlive() && --this.eggTime <= 0) {
+            // VanillaCopy - Spawn chicken egg
+            this.playSound(SoundEvents.CHICKEN_EGG, 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
+            this.spawnAtLocation(Items.EGG);
+            this.gameEvent(GameEvent.ENTITY_PLACE);
+            this.eggTime = this.random.nextInt(6000) + 6000;
         }
 
         this.noActionTime++;
