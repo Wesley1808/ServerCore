da466df84d34958f51d268eaf498a598904f452b
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2022-07-09 11:33:12 +0200

    Fixes thread-safety issues and some other potential mobspawning issues.
    - Also removes a few mobspawning patches that are no longer necessary.

--- src/main/java/me/wesley1808/servercore/mixin/optimizations/mob_spawning/NaturalSpawnerMixin.java
+++ src/main/java/me/wesley1808/servercore/mixin/optimizations/mob_spawning/NaturalSpawnerMixin.java
@@ -4,51 +4,16 @@ import me.wesley1808.servercore.common.utils.ChunkManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.NaturalSpawner;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.chunk.LevelChunk;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(NaturalSpawner.class)
 public abstract class NaturalSpawnerMixin {
-    @Unique
-    private static ChunkAccess cachedChunk;
 
-    @Shadow
-    private static boolean isRightDistanceToPlayerAndSpawnPoint(ServerLevel serverLevel, ChunkAccess chunkAccess, BlockPos.MutableBlockPos mutableBlockPos, double d) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Cancel spawn attempts if they are in unloaded chunks.
-    @Redirect(
-            method = "spawnCategoryForPosition(Lnet/minecraft/world/entity/MobCategory;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkAccess;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/NaturalSpawner$SpawnPredicate;Lnet/minecraft/world/level/NaturalSpawner$AfterSpawnCallback;)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/NaturalSpawner;isRightDistanceToPlayerAndSpawnPoint(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkAccess;Lnet/minecraft/core/BlockPos$MutableBlockPos;D)Z"
-            )
-    )
-    private static boolean onlySpawnIfLoaded(ServerLevel world, ChunkAccess chunk, BlockPos.MutableBlockPos pos, double squaredDistance) {
-        return isRightDistanceToPlayerAndSpawnPoint(world, chunk, pos, squaredDistance) && (cachedChunk = ChunkManager.getChunkIfLoaded(world, pos)) != null;
-    }
-
-    // Sets cached chunk to null after finishing spawn attempts.
-    // This way we guarantee this chunk isn't used for custom spawn logic in other mods, causing weird incompatibilities.
-    @Inject(method = "spawnCategoryForPosition(Lnet/minecraft/world/entity/MobCategory;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkAccess;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/NaturalSpawner$SpawnPredicate;Lnet/minecraft/world/level/NaturalSpawner$AfterSpawnCallback;)V", at = @At("RETURN"))
-    private static void onReturn(MobCategory mobCategory, ServerLevel serverLevel, ChunkAccess chunkAccess, BlockPos blockPos, NaturalSpawner.SpawnPredicate spawnPredicate, NaturalSpawner.AfterSpawnCallback afterSpawnCallback, CallbackInfo ci) {
-        cachedChunk = null;
-    }
-
-    // Fast biome lookups.
     @Redirect(
             method = {"mobsAt", "getRandomSpawnMobAt"},
             require = 0,
@@ -58,43 +23,7 @@ public abstract class NaturalSpawnerMixin {
             )
     )
     private static Holder<Biome> fastBiomeLookup(ServerLevel level, BlockPos pos) {
-        return cachedChunk != null ? ChunkManager.getRoughBiome(cachedChunk, pos) : level.getBiome(pos);
-    }
-
-    // Fast block / fluid state lookups.
-    @Redirect(
-            method = "isInNetherFortressBounds",
-            require = 0,
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/server/level/ServerLevel;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"
-            )
-    )
-    private static BlockState fastBlockStateLookup$1(ServerLevel level, BlockPos pos) {
-        return cachedChunk != null ? cachedChunk.getBlockState(pos) : level.getBlockState(pos);
-    }
-
-    @Redirect(
-            method = "isSpawnPositionOk",
-            require = 0,
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/LevelReader;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"
-            )
-    )
-    private static BlockState fastBlockStateLookup$2(LevelReader level, BlockPos pos) {
-        return cachedChunk != null ? cachedChunk.getBlockState(pos) : level.getBlockState(pos);
-    }
-
-    @Redirect(
-            method = "isSpawnPositionOk",
-            require = 0,
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/LevelReader;getFluidState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/material/FluidState;"
-            )
-    )
-    private static FluidState fastFluidStateLookup(LevelReader level, BlockPos pos) {
-        return cachedChunk != null ? cachedChunk.getFluidState(pos) : level.getFluidState(pos);
+        LevelChunk chunk = ChunkManager.getChunkIfLoaded(level, pos);
+        return chunk != null ? ChunkManager.getRoughBiome(chunk, pos) : level.getBiome(pos);
     }
 }
