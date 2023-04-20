cba2370db89e496da9a3d67b1838c18fbedf807d
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2023-04-20 16:21:04 +0200

    23w16a

--- forge/src/main/java/me/wesley1808/servercore/forge/common/ForgePermissions.java
+++ forge/src/main/java/me/wesley1808/servercore/forge/common/ForgePermissions.java
@@ -1,5 +1,6 @@
 package me.wesley1808.servercore.forge.common;
 
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import me.wesley1808.servercore.common.ServerCore;
 import net.minecraftforge.server.permission.events.PermissionGatherEvent;
 import net.minecraftforge.server.permission.nodes.PermissionNode;
@@ -8,10 +9,10 @@ import net.minecraftforge.server.permission.nodes.PermissionTypes;
 import java.util.Map;
 
 public class ForgePermissions {
-    private static final Map<String, PermissionNode<Boolean>> PERMISSIONS = Map.of(
-            "command.config", node("command.config"),
-            "command.settings", node("command.settings"),
-            "command.statistics", node("command.statistics")
+    private static final Map<String, PermissionNode<Boolean>> PERMISSIONS = ForgePermissions.build(
+            "command.config",
+            "command.settings",
+            "command.statistics"
     );
 
     public static PermissionNode<Boolean> getPermissionNode(String node) {
@@ -24,7 +25,11 @@ public class ForgePermissions {
         }
     }
 
-    private static PermissionNode<Boolean> node(String node) {
-        return new PermissionNode<>(ServerCore.MODID, node, PermissionTypes.BOOLEAN, (x, y, z) -> false);
+    private static Map<String, PermissionNode<Boolean>> build(String... nodes) {
+        Map<String, PermissionNode<Boolean>> permissions = new Object2ObjectOpenHashMap<>();
+        for (String node : nodes) {
+            permissions.put(node, new PermissionNode<>(ServerCore.MODID, node, PermissionTypes.BOOLEAN, (x, y, z) -> false));
+        }
+        return permissions;
     }
 }
