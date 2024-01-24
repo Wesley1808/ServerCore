7d4d255cbf6cdfc850478820d23b1088eaa2275c
Drex <nicknamedrex@gmail.com>
2024-01-24 21:08:16 +0100

    Update to 24w04a

--- common/src/main/java/me/wesley1808/servercore/mixin/features/misc/EntityMixin.java
+++ common/src/main/java/me/wesley1808/servercore/mixin/features/misc/EntityMixin.java
@@ -1,9 +1,13 @@
 package me.wesley1808.servercore.mixin.features.misc;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import me.wesley1808.servercore.common.utils.ChunkManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.levelgen.Heightmap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,14 +17,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class EntityMixin {
 
     // Fall back to default spawn position if the spawn chunks aren't loaded.
-    @Redirect(
+    @WrapOperation(
             method = "findDimensionEntryPoint",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/server/level/ServerLevel;getHeightmapPos(Lnet/minecraft/world/level/levelgen/Heightmap$Types;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/BlockPos;"
+                    target = "Lnet/minecraft/world/level/chunk/LevelChunk;getHeight(Lnet/minecraft/world/level/levelgen/Heightmap$Types;II)I"
             )
     )
-    private BlockPos servercore$fixSpawnHeight(ServerLevel level, Heightmap.Types types, BlockPos blockPos) {
-        return ChunkManager.hasChunk(level, blockPos) ? level.getHeightmapPos(types, blockPos) : blockPos;
+    private int servercore$fixSpawnHeight(LevelChunk instance, Heightmap.Types types, int x, int y, Operation<Integer> original, ServerLevel level, @Local BlockPos blockPos) {
+        return ChunkManager.hasChunk(level, x >> 4, y >> 4) ? original.call(instance, types, x, y) : blockPos.getY();
     }
 }
