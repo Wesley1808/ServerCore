9308ec9a0f1c8573a2ef08f054564534a663144f
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2024-04-06 12:11:40 +0200

    Replace mobcap setting `mobcap-modifier` with `additional-capacity`
    Instead of modifying the mobcap by a modifier additional-capacity will simply add a constant value to it.
    
    Reason for this being that the modifier becomes very inconsistent to use when the mobcap percentage gets modified to anything not 100%.
    
    If the mobcap would be 10% this would basically mean that at a modifier of 1.5x spawners and such would almost never get a chance to spawn as it only gets 3 mobs headroom.

--- common/src/main/java/me/wesley1808/servercore/common/config/data/mob_spawning/EnforcedMobcap.java
+++ common/src/main/java/me/wesley1808/servercore/common/config/data/mob_spawning/EnforcedMobcap.java
@@ -1,7 +1,7 @@
 package me.wesley1808.servercore.common.config.data.mob_spawning;
 
 import space.arim.dazzleconf.annote.ConfDefault.DefaultBoolean;
-import space.arim.dazzleconf.annote.ConfDefault.DefaultDouble;
+import space.arim.dazzleconf.annote.ConfDefault.DefaultInteger;
 import space.arim.dazzleconf.annote.ConfKey;
 import space.arim.dazzleconf.annote.NumericRange;
 import space.arim.dazzleconf.sorter.AnnotationBasedSorter.Order;
@@ -13,8 +13,8 @@ public interface EnforcedMobcap {
     boolean enforcesMobcap();
 
     @Order(2)
-    @ConfKey("mobcap-modifier")
-    @DefaultDouble(1.5)
+    @ConfKey("additional-capacity")
+    @DefaultInteger(32)
     @NumericRange(min = 0)
-    double mobcapModifier();
+    int additionalCapacity();
 }
