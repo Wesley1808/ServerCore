d941ec9d36f47e213cf96cda58913f65fd73d0c1
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2024-04-24 14:41:49 +0200

    Use updated text parser on fabric

--- fabric/src/main/java/me/wesley1808/servercore/fabric/common/FabricMinecraftPlatform.java
+++ fabric/src/main/java/me/wesley1808/servercore/fabric/common/FabricMinecraftPlatform.java
@@ -1,6 +1,7 @@
 package me.wesley1808.servercore.fabric.common;
 
-import eu.pb4.placeholders.api.TextParserUtils;
+import eu.pb4.placeholders.api.ParserContext;
+import eu.pb4.placeholders.api.parsers.NodeParser;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import me.wesley1808.servercore.common.ServerCore;
 import me.wesley1808.servercore.common.services.platform.MinecraftPlatform;
@@ -10,6 +11,11 @@ import net.minecraft.server.level.ChunkMap;
 import net.minecraft.world.level.ChunkPos;
 
 public class FabricMinecraftPlatform implements MinecraftPlatform {
+    private final NodeParser parser = NodeParser.builder()
+            .simplifiedTextFormat()
+            .quickText()
+            .build();
+
     @Override
     public boolean hasPermission(CommandSourceStack source, String node, int level) {
         String permission = String.format("%s.%s", ServerCore.MODID, node);
@@ -23,6 +29,6 @@ public class FabricMinecraftPlatform implements MinecraftPlatform {
 
     @Override
     public Component parseText(String input) {
-        return TextParserUtils.formatText(input);
+        return this.parser.parseText(input, ParserContext.of());
     }
 }
