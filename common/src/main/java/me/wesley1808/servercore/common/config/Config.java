31db10023f536f5b4024240a320aaa5d444efb3c
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2024-09-11 19:11:51 +0200

    Workaround for pre-modloading entity initializations
    Should fix incompatibility with custom name tags.

--- common/src/main/java/me/wesley1808/servercore/common/config/Config.java
+++ common/src/main/java/me/wesley1808/servercore/common/config/Config.java
@@ -17,6 +17,10 @@ public class Config {
         return mainConfigManager.get();
     }
 
+    public static MainConfig getSafe() {
+        return mainConfigManager == null ? null : mainConfigManager.get();
+    }
+
     public static void loadOptimizationConfig() {
         OPTIMIZATION_MANAGER.reload();
     }
